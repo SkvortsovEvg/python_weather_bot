d372e337a5dc828683e646ec574f1da5ef0951a5
Oleg Skvortsov <86718968+llightmoonl@users.noreply.github.com>
2025-01-02 21:39:05 +0300

    Delete weather_bot_001 directory

--- weather_bot_001/hex2rgb.py
+++
@@ -1,2 +0,0 @@
-hex_color = input('Enter hex: ').lstrip('#')
-print('RGB =', tuple(int(hex_color[i:i + 2], 16) for i in (0, 2, 4)))
--- weather_bot_001/sign_temp.py
+++
@@ -1,5 +0,0 @@
-def sign_temperature(temperature):
-    if temperature > 0:
-        return f"+{temperature}°"
-    else:
-        return f"{temperature}°"
